777c25e2a55cee9518afda2ef8c50c84280fba7c
Chrisu <c.bartsch@gmx.at>
2018-02-16 11:02:46 +0100

    Remove swat the fly demo again

--- swatthefly/cacheHack.js
+++
@@ -1,9 +0,0 @@
-/* JavaScript iOS Cache Hack for Wikitude 7.0 */
-function cacheHack () {
-	return cacheHack.random;
-};
-if (navigator.userAgent.match(/Android/i) == null) {
-	cacheHack.random = "?ios=" + Math.floor(Math.random()*99999);
-} else  {
-	cacheHack.random = "";
-}
